6ff64b92bf6f82734aa57469257a6792efd92496
Umi <bo314@naver.com>
2020-07-14 13:07:02 +0900

    Add: makeBadges (#1)
    * Update: ignores
    
    * Add: makeBadges

---
+++ makeBadges/index.js
@@ -0,0 +1,23 @@
+const { makeBadge, ValidationError } = require('badge-maker')
+const yaml = require('yaml')
+const svg2png = require('svg2png')
+const fs = require('fs')
+
+const yamlData = fs.readFileSync('../Chart.yaml', 'utf-8')
+const parsedYaml = yaml.parse(yamlData)
+
+const format = {
+  label: 'version',
+  message: parsedYaml.version,
+  color: 'orange',
+}
+
+const svg = makeBadge(format)
+svg2png(svg)
+  .then(function(buffer){
+    // console.log(buffer)
+    if(!fs.existsSync("./media")){
+      fs.mkdirSync("./media")
+    }
+    fs.writeFileSync("./media/version_info.png", buffer)
+  })
