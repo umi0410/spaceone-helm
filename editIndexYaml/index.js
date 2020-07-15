bbe51722ebd064a538f3960ef58d349c8cbfd0bb
Jinsu Park <bo314@naver.com>
2020-07-15 17:32:14 +0900

    Fix: wrong s3 endpoint

--- editIndexYaml/index.js
+++ editIndexYaml/index.js
@@ -1,5 +1,14 @@
 const fs = require("fs")
+
+S3_BUCKET_NAME = "helm.stargate.spaceone.dev-bucket"
+ALTERNATIVE_HOST = "helm.stargate.spaceone.dev"
+// S3_BUCKET_NAME = process.env.S3_BUCKET_NAME
+// ALTERNATIVE_ENDPOINT = process.env.ALTERNATIVE_ENDPOINT
 let index = fs.readFileSync("index.yaml", "utf-8")
 console.log(index)
-index = index.replace(/- s3:\/\//gi, "- https://")
+
+let reg = new RegExp("- s3://"+S3_BUCKET_NAME, "g")
+index = index.replace(reg, `- https://${ALTERNATIVE_HOST}`)
+console.log(index)
+
 fs.writeFileSync("index.yaml", index)
